da871123bcbb973efaaa4fa8efb1e9ed50c85b02
Juraj Noge <juraj.noge@productboard.com>
2025-12-06 10:26:27 +0100

    Day 6 - clean

--- src/main/kotlin/day6.kt
+++ src/main/kotlin/day6.kt
@@ -3,38 +3,33 @@ import utils.readInputLines
 /** [https://adventofcode.com/2025/day/6] */
 class Spaces : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        val pattern = Regex("([*+]\\s*)")
-        val grid = readInputLines("6.txt")
-        val operators = pattern.findAll(grid.last()).map { it.value }.map { it.first() to it.length - 1 }.toList()
-
-        var offset = 0
-        return operators.sumOf { (operator, size) ->
-            val numbers = grid.dropLast(1).map { row -> row.drop(offset).let { if (size > 0) it.take(size) else it } }
-            offset += size + 1
+        val input = readInputLines("6.txt")
+        val columnIndices = input.maxBy { it.length }.indices
+        val problems =
             if (part2) {
-                val newSize = if (size == 0) numbers.maxOf { it.length } else size
-                val newNumbers =
-                    (0 until newSize).map { idx ->
-                        numbers.mapNotNull { it.getOrNull(idx).takeUnless { it == ' ' } }.joinToString("")
+                val columns =
+                    columnIndices.map { row ->
+                        input.mapNotNull { it.getOrNull(row) }.joinToString("").replace(" ", "")
                     }
-                newNumbers.map { it.trim().toLong() }.reduce { acc, l -> if (operator == '+') acc + l else acc * l }
-            } else {
-                numbers.map { it.trim().toLong() }.reduce { acc, l -> if (operator == '+') acc + l else acc * l }
-            }
-        }
 
-        //        return grid.first().indices.sumOf { column ->
-        //            val rawNumbers = grid.indices.toList().dropLast(1).map { row -> grid[row][column] }
-        //            val operation: ((Long, Long) -> Long) = if (grid[grid.lastIndex][column] == "*") Long::times else
-        // Long::plus
-        //            val numbers =
-        //                if (part2) {
-        //                    emptyList()
-        //                } else {
-        //                    rawNumbers.map(String::toLong)
-        //                }
-        //            numbers.reduce(operation)
-        //        }
+                columns.fold(mutableListOf(mutableListOf<String>())) { groups, column ->
+                    when {
+                        column.isEmpty() -> groups.add(mutableListOf())
+                        else -> groups.last().add(column)
+                    }
+                    groups
+                }
+            } else {
+                val processedInput = input.map { row -> row.trim().split("\\s+".toRegex()) }
+                columnIndices.map { row -> processedInput.mapNotNull { it.getOrNull(row) } }.filter { it.isNotEmpty() }
+            }
+
+        return problems.sumOf { problem ->
+            val operator = if (part2) problem.first().last().toString() else problem.last()
+            problem
+                .mapNotNull { it.filter(Char::isDigit).toLongOrNull() }
+                .reduce { a, b -> if (operator == "+") a + b else a * b }
+        }
     }
 }
 
