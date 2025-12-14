0b9cc26cd01412a88ff72b08ef0b52b5e67f69ff
Juraj Noge <juraj.noge@productboard.com>
2025-12-14 20:22:30 +0100

    Day 9

--- src/main/kotlin/day9.kt
+++ src/main/kotlin/day9.kt
@@ -1,11 +1,10 @@
-import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 import utils.Coordinate
 import utils.readInputLines
 
 /** [https://adventofcode.com/2025/day/9] */
-class Day9 : AdventOfCodeTask {
+class Rectangles : AdventOfCodeTask {
 
     override fun run(part2: Boolean): Any {
         val corners =
@@ -14,64 +13,37 @@ class Day9 : AdventOfCodeTask {
                 Coordinate(x, y)
             }
 
-        data class Edge(val c1: Coordinate, val c2: Coordinate) {
-            val minX = min(c1.x, c2.x)
-            val maxX = max(c1.x, c2.x)
-            val minY = min(c1.y, c2.y)
-            val maxY = max(c1.y, c2.y)
+        fun IntRange.overlaps(other: IntRange): Boolean = max(first, other.first) <= min(last, other.last)
+        fun IntRange.size(): Int = last - first + 1
+
+        data class Rectangle(val c1: Coordinate, val c2: Coordinate) {
+            val xRange = min(c1.x, c2.x)..max(c1.x, c2.x)
+            val yRange = min(c1.y, c2.y)..max(c1.y, c2.y)
+
+            fun inner(): Rectangle =
+                Rectangle(Coordinate(xRange.first + 1, yRange.first + 1), Coordinate(xRange.last - 1, yRange.last - 1))
+
+            fun overlaps(other: Rectangle): Boolean = xRange.overlaps(other.xRange) && yRange.overlaps(other.yRange)
+
+            fun area(): Long = xRange.size().toLong() * yRange.size()
         }
 
-        val polygon = (corners + corners.first()).zipWithNext().map { (c1, c2) -> Edge(c1, c2) }.toSet()
-        val rectangles = corners.flatMapIndexed { offset, c1 -> corners.drop(offset).map { c2 -> Edge(c1, c2) } }
+        val polygon = (corners + corners.first()).zipWithNext().map { (c1, c2) -> Rectangle(c1, c2) }
+        val rectangles = corners.flatMapIndexed { offset, c1 -> corners.drop(offset).map { c2 -> Rectangle(c1, c2) } }
 
-        fun Edge.hasLineInside(line: Edge): Boolean =
-            if (line.c1.x == line.c2.x && line.c1.x in (minX + 1 until maxX)) {
-                line.minY <= minY && line.maxY > minY || line.maxY >= maxY && line.minY < maxY
-            } else if (line.c1.y == line.c2.y && line.c1.y in (minY + 1 until maxY)) {
-                line.minX <= minX && line.maxX > maxX || line.maxX >= maxX && line.minX < maxX
-            } else {
-                false
-            }
-
-        fun Edge.isInsidePolygon(): Boolean {
-
-            val centerPoint = Coordinate(minX + 1, minY + 1)
-
-            return polygon.any { centerPoint.x in it.minX..it.maxX && it.maxY < centerPoint.y } &&
-                polygon.any { centerPoint.x in it.minX..it.maxX && it.minY > centerPoint.y } &&
-                polygon.any { centerPoint.y in it.minY..it.maxY && it.maxX < centerPoint.x } &&
-                polygon.any { centerPoint.y in it.minY..it.maxY && it.minX > centerPoint.x }
-        }
-
-        val (valid, invalid) =
-            rectangles.partition { rectangle ->
+        return rectangles
+            .filter { rectangle ->
                 if (part2) {
-                    polygon.none { border -> rectangle.hasLineInside(border) } && rectangle.isInsidePolygon()
+                    val inner = rectangle.inner()
+                    polygon.none { border -> inner.overlaps(border) }
                 } else {
                     true
                 }
             }
-
-        var counter = 0
-        return valid
-            .map { (c1, c2) -> Edge(c1, c2) to (abs(c1.x - c2.x) + 1).toLong() * (abs(c1.y - c2.y) + 1) }
-            .sortedByDescending { it.second }
-            .first { (edge, area) ->
-                println("NOPE $area")
-                (edge.minX..edge.maxX).all { x ->
-                    (edge.minY..edge.maxY).all { y ->
-                        val centerPoint = Coordinate(x, y)
-                        polygon.any { centerPoint.x in it.minX..it.maxX && it.maxY <= centerPoint.y } &&
-                            polygon.any { centerPoint.x in it.minX..it.maxX && it.minY >= centerPoint.y } &&
-                            polygon.any { centerPoint.y in it.minY..it.maxY && it.maxX <= centerPoint.x } &&
-                            polygon.any { centerPoint.y in it.minY..it.maxY && it.minX >= centerPoint.x }
-                    }
-                }
-            }
-            .second
+            .maxOf(Rectangle::area)
     }
 }
 
 fun main() {
-    print(Day9().run(part2 = true))
+    print(Rectangles().run(part2 = true))
 }
